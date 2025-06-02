a6481543d57a1aca3a0f90c94e75b29ac18e7005
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-06-02 14:38:32 -0300

    feat: :recycle: Se crearon variables para los colores, el logo ahora es absolute y se le ajusto el tamaño, se hicieron otros cambios esteticos

--- src/components/NavbarDesktop.tsx
+++ src/components/NavbarDesktop.tsx
@@ -12,17 +12,17 @@ import {
 export function NavbarDesktop() {
 
     return (
-        <NavigationMenu viewport={false}>
+        <NavigationMenu viewport={false} className="hidden md:flex w-full items-center justify-center">
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <a href="/" className="flex items-center gap-2">
+                    <NavigationMenuLink className="text-lg font-medium hover:bg-[var(--color-complementario)]" asChild>
+                        <a href="/" className="flex items-center gap-2 ">
                             Inicio
                         </a>
                     </NavigationMenuLink>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
+                    <NavigationMenuLink className="text-lg font-medium hover:bg-[var(--color-complementario)]" asChild>
                         <a href="/productos" className="flex items-center gap-2">
                             Productos
                         </a>
@@ -30,22 +30,22 @@ export function NavbarDesktop() {
                 </NavigationMenuItem>
 
                 <NavigationMenuItem >
-                    <NavigationMenuTrigger>Kurbis</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink>
+                    <NavigationMenuTrigger className="text-lg font-medium hover:bg-[var(--color-complementario)] data-[state=open]:bg-[var(--color-complementario)] data-[state=open]:hover:bg-[var(--color-complementario)] data-[state=open]:focus:bg-[var(--color-complementario)]">Kurbis</NavigationMenuTrigger>
+                    <NavigationMenuContent >
+                        <NavigationMenuLink className="text-md hover:bg-[var(--color-complementario)]">
                             <a href="/mision">Mision</a>
                         </NavigationMenuLink>
-                        <NavigationMenuLink>
+                        <NavigationMenuLink className="text-md hover:bg-[var(--color-complementario)]">
                             <a href="/sustentabilidad">Sustentabilidad</a>
                         </NavigationMenuLink>
-                        <NavigationMenuLink>
+                        <NavigationMenuLink className="text-md hover:bg-[var(--color-complementario)]">
                             <a href="/nosotros">Nosotros</a>
                         </NavigationMenuLink>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
+                    <NavigationMenuLink className="text-lg font-medium hover:bg-[var(--color-complementario)]" asChild>
                         <a href="/recetas" className="flex items-center gap-2">
                             Recetas
                         </a>
