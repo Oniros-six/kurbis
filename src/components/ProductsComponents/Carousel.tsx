652e48f13019a2a57f4f2b339e3cb6e868ba272f
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-06-27 14:55:35 -0300

    refactor: :recycle: Quite el recuadro negro del carousel

--- src/components/ProductsComponents/Carousel.tsx
+++ src/components/ProductsComponents/Carousel.tsx
@@ -1,28 +1,14 @@
 import Autoplay from "embla-carousel-autoplay"
 import { Carousel, CarouselContent, CarouselItem, } from "@/components/ui/carousel"
-import { useState, useCallback, type SetStateAction } from "react"
 
 type content = {
   imagen: string
   alt: string
-  withInfo: boolean
-  info?: string
 }
 
 export function CarouselProduct({ contenido }: { contenido: content[] }) {
-  const [currentSlide, setCurrentSlide] = useState(0)
 
   const plugin = Autoplay({ delay: 3000, stopOnInteraction: true })
-
-  const onSlideChange = useCallback((api: { selectedScrollSnap: () => SetStateAction<number> }) => {
-    if (!api) return
-    setCurrentSlide(api.selectedScrollSnap())
-  }, [])
-
-  // Verificar si el slide actual tiene withInfo: true
-  const shouldShowInfo = contenido[currentSlide]?.withInfo || false
-  const info = shouldShowInfo ? contenido[currentSlide].info : ""
-
   return (
     <section className="p-4 lg:py-16 lg:px-4 bg-[var(--color-background-alt)]">
       <div className="max-w-100 mx-auto fade-in-up">
@@ -31,15 +17,6 @@ export function CarouselProduct({ contenido }: { contenido: content[] }) {
             opts={{ loop: true, align: "start" }}
             plugins={[plugin]}
             className="relative"
-            setApi={(api) => {
-              if (!api) return
-              
-              // Establecer slide inicial
-              setCurrentSlide(api.selectedScrollSnap())
-              
-              // Escuchar cambios de slide
-              api.on('select', () => onSlideChange(api))
-            }}
           >
             <CarouselContent>
               {contenido.map((item, index) => (
@@ -54,19 +31,6 @@ export function CarouselProduct({ contenido }: { contenido: content[] }) {
                 </CarouselItem>
               ))}
             </CarouselContent>
-            
-            {/* Mostrar el div con transición desde la esquina */}
-            <div 
-              className={`absolute flex justify-center items-center size-25 border-[var(--color-primario)] border-1 bg-black backdrop-blur-sm shadow-lg transition-all duration-200 ease-out ${
-                shouldShowInfo 
-                  ? '-top-10 -right-10 opacity-100 scale-100' 
-                  : 'top-0 right-0 opacity-0 scale-75 pointer-events-none'
-              }`}
-            >
-              <span className="text-xs font-semibold text-center leading-tight text-[var(--color-primario)]">
-                {info}
-              </span>
-            </div>
           </Carousel>
         </div>
       </div>
