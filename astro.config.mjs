f55aacff2c8cd7e05231645a9f3233fa8cc35149
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-07-18 10:27:41 -0300

    feat: :sparkles: Agregue el sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 import tunnel from 'astro-tunnel';
-// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), tunnel()],
+  integrations: [react(), tunnel(), sitemap()],
+  site: "https://kurbis.uy",
 });
