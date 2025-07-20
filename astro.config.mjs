b72cf70397dc06ddf7b00e4b1c713e56df6b8e78
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-07-19 22:44:32 -0300

    feat: :sparkles: Actualizamos el sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://www.kurbis.com.uy",
   integrations: [react(), tunnel(), sitemap()],
-  site: "https://kurbis.uy",
 });
